6b7a6940b82920a0ee5a681cf98cc767c53f7ee8
Liubomyr <liubomyrromankiv@gmail.com>
2017-07-26 17:36:49 +0300

    navigation to homepage after submit post added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 	renderField(field) {
@@ -20,7 +23,10 @@ class PostsNew extends Component {
 		);
 	}
 	onSubmit(values) {
-		console.log(values);
+		
+		this.props.createPost(values, () => {
+			this.props.history.push('/');
+		});
 	}
 	render(){
 		const { handleSubmit } = this.props;
@@ -42,7 +48,8 @@ class PostsNew extends Component {
 					name="content" 
 					component={this.renderField}
 				/>
-				<button type="submit">Submit</button>
+				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
@@ -68,4 +75,6 @@ function validate(values){
 export default reduxForm({
 	validate,
 	form: 'PostNewForm'
-})(PostsNew);
+})(
+	connect(null,{ createPost })(PostsNew)
+);
