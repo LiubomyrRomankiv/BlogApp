669970f4fa7da34f34033f8bea3c94ea838fe811
Liubomyr <liubomyrromankiv@gmail.com>
2017-07-26 13:35:05 +0300

    new post component added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reduser as formReduser } from 'redux-form';
 import PostsReduser from './reduser_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReduser
+  posts: PostsReduser,
+  // form: formReduser
 });
 
 export default rootReducer;
